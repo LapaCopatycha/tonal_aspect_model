e7a3542be2e821071e3a1c92cb892accabdf219f
admin <@admin>
2025-05-09 13:55:14 +0300

    Init the use.py. (It need to use the model after fit, training)

---
+++ taa_model/model/use.py
@@ -0,0 +1,45 @@
+import csv
+import torch
+import torch.nn.functional as F
+
+from paths import BASE_DIR
+from taa_model.model.model import model, navec
+from taa_model.model.preparation import prep_review
+
+
+def mark_review(review):
+    model_path = f"{BASE_DIR}/taa_model/model/model_rnn_bidir.tar"
+    st = torch.load(model_path, weights_only=True)
+    model.load_state_dict(st)
+
+    # Вообще надо подумать как это закешировать так на загрузку этих заголовков будет тратиться очень много времени
+    headers_path = f'{BASE_DIR}/taa_model/data/prepared_reviews.csv'
+    with open(headers_path, 'r', encoding='utf-8') as f:
+        reader = csv.reader(f, delimiter='\t')
+        header = list(reader)[0]
+
+    # phrase_lst = review.lower().split()
+    # phrase_lst = [torch.tensor(navec[w]) for w in phrase_lst if w in navec]
+    phrase_lst = prep_review(phrase=review, navec_emb=navec)
+    _data_batch = torch.stack(phrase_lst)
+
+
+    predict = model(_data_batch.unsqueeze(0)).squeeze(0)
+    p = F.sigmoid(predict)
+    y = (p>0.5).int()
+
+    print(header)
+    print(predict)
+    print((p>0.5).int())
+
+    return {name: int(y[i]) for i, name in enumerate(header[:-1])}
+
+def mark_review_lst (reviews:list):
+    return [mark_review(review) for review in reviews]
+
+# phrase = "Этот телефон не работает. Батарейку не держит. Упакованно ужасно"
+# phrase = "Этот телефон работает. Батарейку держит. Упакованно круто"
+# print(mark_review(review=phrase))
+
+# Нужно будет подумать над функцей активации так как есть проблема в том что два противоречивых класса могут выпасть
+# Если оба больше 0.5 выбирается масксимальный из двух
