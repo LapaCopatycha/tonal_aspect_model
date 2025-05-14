fd27cf1a2980159551fbdd4b4791b3590c649c94
admin <@admin>
2025-05-14 19:15:57 +0300

    test.py was cleaned code

--- taa_model/model/test.py
+++ taa_model/model/test.py
@@ -1,19 +1,7 @@
 import csv
 import numpy as np
 
-import torch.utils.data as data
-from model import PhraseDataset, model, navec
-from taa_model.model.metrics import model_eval
 from taa_model.model.use import mark_review
-
-# Что то тут ни как нормально не работает
-# def t_model():
-#     test_dataset = PhraseDataset("../data/test.csv", navec)
-#     test_data = data.DataLoader(test_dataset, batch_size=1, shuffle=False)
-#     test_metrics = model_eval(model, test_data, loss_func=False)
-#     return test_metrics
-#
-# print(t_model())
 
 def t_model():
     path = '../data/test.csv'
@@ -34,5 +22,3 @@ def t_model():
         all_len += len(real_marks)
     hamming_loss = 1 - (all_positive / all_len)
     return hamming_loss
-
-print(t_model())
