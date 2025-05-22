656dbf1a15ff8cf242d05f7ae801a424e3390d7e
admin <@admin>
2025-05-23 00:04:02 +0300

    Add new metrics

--- taa_model/model/metrics.py
+++ taa_model/model/metrics.py
@@ -1,5 +1,55 @@
 import torch
 import torch.nn.functional as F
+from sympy.physics.units import micro
+
+def update_crosstab(crosstab, p, y):
+    crosstab['true_positive'] += ((p == 1) & (y == 1)).sum(dim=0)
+    crosstab['false_positive'] += ((p == 1) & (y == 0)).sum(dim=0)
+    crosstab['false_negative'] += ((p == 0) & (y == 1)).sum(dim=0)
+    crosstab['true_negative'] += ((p == 0) & (y == 0)).sum(dim=0)
+    return crosstab
+
+def macro_averange_metrics(crosstab, prefix=''):
+    true_positive = crosstab['true_positive']
+    false_positive = crosstab['false_positive']
+    false_negative = crosstab['false_negative']
+    true_negative = crosstab['true_negative']
+
+    e = 0.001
+    precision = true_positive / (true_positive + false_positive + e)
+    recall = true_positive / (true_positive + false_negative+ e)
+    accuracy = (true_positive + true_negative) / (true_positive + false_positive + false_negative + true_negative)
+
+    f1 = 2 * (precision * recall) / (precision + recall + e)
+
+    precision = precision.mean().item()
+    recall = recall.mean().item()
+    accuracy = accuracy.mean().item()
+    f1 = f1.mean().item()
+    return {f'{prefix}precision' : precision, f'{prefix}recall' : recall,
+            f'{prefix}accuracy' : accuracy, f'{prefix}f1' : f1}
+
+def micro_averange_metrics(crosstab, prefix=''):
+    true_positive = crosstab['true_positive']
+    false_positive = crosstab['false_positive']
+    false_negative = crosstab['false_negative']
+    true_negative = crosstab['true_negative']
+
+    e = 0.001
+    # Микро среднее:
+    precision = true_positive.sum() / (true_positive.sum() + false_positive.sum() + e)
+    recall = true_positive.sum() / (true_positive.sum() + false_negative.sum() + e)
+    accuracy = ((true_positive.sum() + true_negative.sum()) /
+                (true_positive.sum() + false_positive.sum() + false_negative.sum() + true_negative.sum()))
+
+    f1 = 2 * (precision * recall) / (precision + recall)
+
+    precision = precision.item()
+    recall = recall.item()
+    accuracy = accuracy.item()
+    f1 = f1.item()
+    return {f'{prefix}precision' : precision, f'{prefix}recall' : recall,
+            f'{prefix}accuracy' : accuracy, f'{prefix}f1' : f1}
 
 # Мб переименовать функцию
 def model_eval(model, data, loss_func):
@@ -9,31 +59,34 @@ def model_eval(model, data, loss_func):
 
     if loss_func is False:
         Q_val = None
-    total_incorrect_hl = 0
-    total_incorrect_mhl = 0
-    total_samples = 0
+
+    crosstab = { 'true_positive' : 0, 'false_positive' : 0,
+                'false_negative' : 0, 'true_negative' : 0}
+
+    modified_crosstab = crosstab.copy()
 
     for x, y in data:
         with torch.no_grad():
             predict = model(x.squeeze(0)).squeeze(0)
+            y = y.squeeze(0)
 
             if loss_func is not False:
-                loss = loss_func(predict, y.squeeze(0))
+                loss = loss_func(predict, y)
                 Q_val += loss.item()
                 count_val += 1
 
-
-            total_incorrect_hl += (((predict)>0.5).int() != y.squeeze(0)).sum().item()
-            total_incorrect_mhl += (features_activation_f(predict) != y.squeeze(0)).sum().item()
-            total_samples += y.squeeze(0).size(dim=0) * y.squeeze(0).size(dim=1)
-            # Как то топорно
-            # мб есть лучше посчитать колличество элементов
+            p = ((predict)>0.5).int()
+            modified_p = features_activation_f(predict)
+            crosstab = update_crosstab(crosstab, p, y)
+            modified_crosstab = update_crosstab(modified_crosstab, modified_p, y)
 
     if loss_func is not False:
         Q_val /= count_val
-    hamming_loss = total_incorrect_hl / total_samples
-    modifed_hamming_loss = total_incorrect_mhl / total_samples
-    return {'Q_val':Q_val, 'hamming_loss' : hamming_loss, 'modifed_hamming_loss' : modifed_hamming_loss}
+
+    metrics = macro_averange_metrics(crosstab)
+    modified_metrics = macro_averange_metrics(modified_crosstab, prefix='modified_')
+
+    return {'Q_val':Q_val, **metrics, **modified_metrics}
 
 def features_activation_f(predict, activate=0.5 , q_features=7):
     new_prediction = []
